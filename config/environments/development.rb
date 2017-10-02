76223df139626a5409d181a9e74e11b084ee4b96
Dave <dr6@sanger.ac.uk>
2017-10-02 11:37:49 +0100

    Update development.rb
    leave fake_ldap = true in development.rb
    We don't need people to put in their real passwords (over http) in development mode.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.fake_ldap = false
+  config.fake_ldap = true
 
 
   config.x.jwt = {
