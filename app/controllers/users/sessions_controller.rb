328385265b401c8a13103dfe91c5055daa308018
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-10-02 11:23:21 +0100

    Append email address to usernames when logging in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,9 +7,13 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    if params.dig(:user, :email).present? && (params[:user][:email].exclude? "@")
+      params[:user][:email] << "@sanger.ac.uk"
+      puts params[:user][:email]
+    end
+    super
+  end
 
   # DELETE /resource/sign_out
   # def destroy
