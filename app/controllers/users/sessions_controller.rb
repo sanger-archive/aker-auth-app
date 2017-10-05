251ec4b10d3d324198dc018ae5cca956f372565e
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-10-05 16:06:14 +0100

    Add support for renewing JWT

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,8 @@
 class Users::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
+  skip_before_action :verify_authenticity_token, only: [:renew_jwt]
+
+  @@secret_key = Rails.application.config.jwt_secret_key
 
   # GET /resource/sign_in
   # def new
@@ -12,13 +15,32 @@ class Users::SessionsController < Devise::SessionsController
       params[:user][:email] << "@sanger.ac.uk"
     end
     super
-    set_jwt_cookie({"email": params[:user][:email]})
+    set_session_cookie
+    jwt = prepare_jwt_cookie({"email": params[:user][:email], "groups": current_user.groups})
+    cookies[:aker_user] = JWT.encode jwt, @@secret_key, 'HS256'
   end
 
   # DELETE /resource/sign_out
   def destroy
     super
     cookies.delete :aker_user
+    cookies.delete :aker_auth_session
+  end
+
+  def renew_jwt
+    # TODO: Check session is valid
+    # Renew JWT if so
+    # TODO: (and update expiry date on refresh token)
+    # Otherwise, unauthorized error
+    user_data = JSON.parse(request.body.read)
+
+    unless user_data.empty?
+      jwt = prepare_jwt_cookie({email: user_data["email"], groups: user_data["groups"]})
+      response = JWT.encode jwt, @@secret_key, 'HS256'
+      render json: response
+    else
+      render body: "JWT from cookie has expired", status: :unauthorized
+    end
   end
 
   def default
@@ -26,13 +48,19 @@ class Users::SessionsController < Devise::SessionsController
 
   private
 
-  def set_jwt_cookie(auth_hash)
-    secret_key = Rails.application.config.jwt_secret_key
+  def prepare_jwt_cookie(auth_hash)
     iat = Time.now.to_i
     exp = Time.now.to_i + Rails.application.config.jwt_exp_time
     nbf = Time.now.to_i - Rails.application.config.jwt_nbf_time
     payload = { data: auth_hash, exp: exp, nbf: nbf, iat: iat }
-    cookies[:aker_user] = JWT.encode payload, Rails.application.config.jwt_secret_key, 'HS256'
+  end
+
+  def set_session_cookie
+    # TODO: Encrypt this once its use is known
+    cookies[:aker_auth_session] = {
+      expires: 1.month.from_now,
+      value: JSON.generate({email: current_user.email, groups: current_user.groups})
+    }
   end
 
   protected
