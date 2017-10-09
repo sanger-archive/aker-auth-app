ba92ff62b630b79b373ee81256205e3735ff8e9f
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-10-09 13:36:11 +0100

    Finish auth service

--- db/migrate/20170919110706_devise_create_users.rb
+++ db/migrate/20170919110706_devise_create_users.rb
@@ -3,14 +3,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      # t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       # t.string   :reset_password_token
       # t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      # t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
