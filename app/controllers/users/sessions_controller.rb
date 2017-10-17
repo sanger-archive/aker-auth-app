1c673b8a9774fa529b7441eac571558ebc25cae2
David Robinson <dr6@sanger.ac.uk>
2017-10-17 09:12:26 +0100

    Allow logout to get past authenticity check so people can log out

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
-  skip_before_action :verify_authenticity_token, only: [:renew_jwt]
+  skip_before_action :verify_authenticity_token, only: [:renew_jwt, :destroy]
 
   # POST /resource/sign_in
   def create
