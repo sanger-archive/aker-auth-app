c44cbec00719f5d3b3e48b1f3c417d02039958b6
Chris Smith <cs24@sanger.ac.uk>
2017-11-06 16:25:49 +0000

    Removed unused initialiser

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20171006123303) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
