f019b4e9485bd996fbfaf52836e3f6909f6e6fda
Chris Smith <chrissmith987@gmail.com>
2017-12-13 11:37:39 +0000

    Add Brakeman (#17)
    * Add Brakeman
    
    * Add .ruby-version file
    
    * Fixed up brakeman warning
    
    Was saying there was a possible sql injection vulnerability in session model
    
    * Add rcov

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,6 @@
 class Session < ApplicationRecord
   def self.sweep(time = 5.days)
-    Session.where("created_at < '#{time.ago.to_s(:db)}'").delete_all
+    Session.where("created_at < ?", time.ago.to_s(:db)).delete_all
     Rails.logger.info("Old sessions purged")
   end
 end
